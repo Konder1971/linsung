152b84325f784681fac4c66878f95a985fc8a344
Konder <konder1971@mail.ru>
2024-11-08 15:47:58 +0200

    hyrfghfghg
    fhgfdhdf

--- js/main.js
+++ js/main.js
@@ -1,79 +1,105 @@
 (function ($) {
-	if (isMac()) {
-		$('body').addClass('mac');
-	} else if (isiOS()) {
-		$('body').addClass('ios');
-	}
+  $(document).ready(function () {
+    
+    // Функция для изменения высоты изображений
+    function resizeImages() {
+      $('.eimage-link img').each(function () {
+        $(this).css('height', $(this).width() + 'px');
+      });
+      $('.portfolio-video').each(function () {
+        $(this).css('height', $(this).width() + 'px');
+      });
+    }
 
-	$('.nav-toggle').click(function () {
-		if ($('.nav-toggle').hasClass('active')) {
-			$('.nav-toggle.active').removeClass('active');
-			$('.navigations.active').removeClass('active');
-			//$('.navlink.active').removeClass('active');
-		}
-		else {
-			$('.nav-toggle').addClass('active');
-			$('.navigations').addClass('active');
-		}
-	});
+    // Начальный вызов для установки высоты изображений
+    resizeImages();
 
-  $('nav li > span').click(function () {
-    $('.nav-toggle.active').removeClass('active');
-    $('.navigations.active').removeClass('active');
-    //$('.navlink.active').removeClass('active');
+    // Вызов функции при изменении размера окна
+    $(window).resize(resizeImages);
+    
   });
-
-	let top = $('.navigations');
-	$(window).scroll(function() {
-		if($(this).scrollTop() > 96) {
-			top.addClass('navigations_fixed');
-		} else {
-			top.removeClass('navigations_fixed');
-		}
-	});
-
-	document.addEventListener('DOMContentLoaded', () => {
-		let toTopBtn = document.querySelector('.to-up');
-		window.onscroll = function () {
-			if (window.pageYOffset > 400) {
-				toTopBtn.style.display = 'block'
-			} else {
-				toTopBtn.style.display = 'none'
-			}
-		}
-		toTopBtn.addEventListener('click', function () {
-			window.scrollBy({
-				top: -document.documentElement.scrollHeight,
-				behavior: 'smooth'
-			});
-		});
-	});
-
-	function isMac() {
-		return navigator.platform.indexOf('Mac') > -1
-	}
-	function isWindows() {
-		return navigator.platform.indexOf('Win') > -1
-	}
-	function isiOS() {
-		return /(iPhone|iPod|iPad)/i.test(navigator.platform) && !window.MSStream;
-	}
-
 })(jQuery);
 
 
 
+
+
+//(function ($) {
+//	if (isMac()) {
+//		$('body').addClass('mac');
+//	} else if (isiOS()) {
+//		$('body').addClass('ios');
+//	}
+
+//	$('.nav-toggle').click(function () {
+//		if ($('.nav-toggle').hasClass('active')) {
+//			$('.nav-toggle.active').removeClass('active');
+//			$('.navigations.active').removeClass('active');
+//			//$('.navlink.active').removeClass('active');
+//		}
+//		else {
+//			$('.nav-toggle').addClass('active');
+//			$('.navigations').addClass('active');
+//		}
+//	});
+
+//  $('nav li > span').click(function () {
+//    $('.nav-toggle.active').removeClass('active');
+//    $('.navigations.active').removeClass('active');
+//    //$('.navlink.active').removeClass('active');
+//  });
+
+//	let top = $('.navigations');
+//	$(window).scroll(function() {
+//		if($(this).scrollTop() > 96) {
+//			top.addClass('navigations_fixed');
+//		} else {
+//			top.removeClass('navigations_fixed');
+//		}
+//	});
+
+//	document.addEventListener('DOMContentLoaded', () => {
+//		let toTopBtn = document.querySelector('.to-up');
+//		window.onscroll = function () {
+//			if (window.pageYOffset > 400) {
+//				toTopBtn.style.display = 'block'
+//			} else {
+//				toTopBtn.style.display = 'none'
+//			}
+//		}
+//		toTopBtn.addEventListener('click', function () {
+//			window.scrollBy({
+//				top: -document.documentElement.scrollHeight,
+//				behavior: 'smooth'
+//			});
+//		});
+//	});
+
+//	function isMac() {
+//		return navigator.platform.indexOf('Mac') > -1
+//	}
+//	function isWindows() {
+//		return navigator.platform.indexOf('Win') > -1
+//	}
+//	function isiOS() {
+//		return /(iPhone|iPod|iPad)/i.test(navigator.platform) && !window.MSStream;
+//	}
+
+//})(jQuery);
+
+
+
   // navigationsFixed start
-  document.addEventListener('DOMContentLoaded', () => {
-    let navigationsFixed = document.querySelector('body');
-    window.onscroll = function () {
-      if (window.pageYOffset > 120) {
-        navigationsFixed.classList.add('navigationsFixed')
-      } else {
-        navigationsFixed.classList.remove('navigationsFixed')
-      }
-    }
-  });
+  //document.addEventListener('DOMContentLoaded', () => {
+  //  let navigationsFixed = document.querySelector('body');
+  //  window.onscroll = function () {
+  //    if (window.pageYOffset > 120) {
+  //      navigationsFixed.classList.add('navigationsFixed')
+  //    } else {
+  //      navigationsFixed.classList.remove('navigationsFixed')
+  //    }
+  //  }
+  //});
   // navigationsFixed stop
 
 
