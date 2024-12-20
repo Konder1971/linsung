2070ddf8fb4a790c381599e4e8745042c8c008c7
Konder <konder1971@mail.ru>
2024-12-20 21:08:53 +0200

    смисми
    смисмисчм

--- js/main.js
+++ js/main.js
@@ -122,3 +122,20 @@
 
   });
 })(jQuery);
+
+document.addEventListener('DOMContentLoaded', () => {
+  let toTopBtn = document.querySelector('.to-up');
+  window.onscroll = function () {
+    if (window.pageYOffset > 400) {
+      toTopBtn.style.display = 'block'
+    } else {
+      toTopBtn.style.display = 'none'
+    }
+  }
+  toTopBtn.addEventListener('click', function () {
+    window.scrollBy({
+      top: -document.documentElement.scrollHeight,
+      behavior: 'smooth'
+    });
+  });
+});
