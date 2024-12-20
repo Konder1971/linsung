9531083cd3211360fbeaff08ad0e9696f6873df6
Konder <konder1971@mail.ru>
2024-12-20 21:12:02 +0200

    выапывп
    вапвыапв

--- js/main.js
+++ js/main.js
@@ -123,19 +123,19 @@
   });
 })(jQuery);
 
-document.addEventListener('DOMContentLoaded', () => {
-  let toTopBtn = document.querySelector('.to-up');
-  window.onscroll = function () {
-    if (window.pageYOffset > 400) {
-      toTopBtn.style.display = 'block'
-    } else {
-      toTopBtn.style.display = 'none'
-    }
-  }
-  toTopBtn.addEventListener('click', function () {
-    window.scrollBy({
-      top: -document.documentElement.scrollHeight,
-      behavior: 'smooth'
-    });
-  });
-});
+//document.addEventListener('DOMContentLoaded', () => {
+//  let toTopBtn = document.querySelector('.to-up');
+//  window.onscroll = function () {
+//    if (window.pageYOffset > 400) {
+//      toTopBtn.style.display = 'block'
+//    } else {
+//      toTopBtn.style.display = 'none'
+//    }
+//  }
+//  toTopBtn.addEventListener('click', function () {
+//    window.scrollBy({
+//      top: -document.documentElement.scrollHeight,
+//      behavior: 'smooth'
+//    });
+//  });
+//});
